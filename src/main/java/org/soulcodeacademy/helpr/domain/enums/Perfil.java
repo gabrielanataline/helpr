8289742c14a605cf8a8cc32a9e7748fc688b7097
gabrielanataline <gabiesnataline@hotmail.com>
2022-11-21 11:44:55 -0300

    Ponte entre banco de dados e spring security

--- src/main/java/org/soulcodeacademy/helpr/domain/enums/Perfil.java
+++ src/main/java/org/soulcodeacademy/helpr/domain/enums/Perfil.java
@@ -1,7 +1,24 @@
 package org.soulcodeacademy.helpr.domain.enums;
+// ROLE = papel do usuario
 
 public enum Perfil {
-    ADMIN,
-    FUNCIONARIO,
-    CLIENTE
+    ADMIN("ROLE_ADMIN"), // 0
+    FUNCIONARIO("ROLE_FUNCIONARIO"), // 1
+    CLIENTE("ROLE_CLIENTE"); // 2
+
+    private String descricao;
+
+    Perfil(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
 }
+
+//public enum Perfil {
+//    ADMIN,
+//    FUNCIONARIO,
+//    CLIENTE
+//}
